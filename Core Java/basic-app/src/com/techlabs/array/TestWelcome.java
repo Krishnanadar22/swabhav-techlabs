5ec691e2b1c821c24c05bde52b5104eff17ab4e8
Sivaramakrishnan Nadar <sivaramakrishnannadar@gmail.com>
2017-12-26 18:47:32 +0530

    refactored TestWeclome

--- Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
+++ Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
@@ -1,5 +1,13 @@
 package com.techlabs.array;
 
 public class TestWelcome {
-	
+	public static void main(String names[]) {
+		if (names.length == 0)
+			System.out.println("Please pass some names");
+		else {
+			for (String n : names) {
+				System.out.println("hi " + n);
+			}
+		}
+	}
 }
